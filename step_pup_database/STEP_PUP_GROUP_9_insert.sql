6cefae58c05ce99c9402ad1b53dcc2775483c813
Andrea Schoonover <arschoon@asu.edu>
2022-08-07 19:07:05 -0400

    initial commit, added sql src files

---
+++ step_pup_database/STEP_PUP_GROUP_9_insert.sql
@@ -0,0 +1,33 @@
+
+INSERT INTO CUSTOMER VALUES ('customer1@email.com','Michael', 'J', 'Scott', '123456789123456', '123 Main Street');
+INSERT INTO CUSTOMER VALUES ('customer2@email.com','Dwight', 'K', 'Schrute', '9876543287678765', '456 Central Street');
+INSERT INTO CUSTOMER VALUES ('customer3@email.com','Pam', 'M', 'Beasley', '6543219876543456', '789 Red Street');
+INSERT INTO CUSTOMER VALUES ('customer4@email.com','Jim', 'J', 'Halpert', '5378629356729367', '123 Blue Street');
+
+INSERT INTO DOG VALUES('customer1@email.com',11, 'Lab', 'Spot', 4);
+INSERT INTO DOG VALUES('customer1@email.com',9, 'Pitbull', 'Gus', 5);
+INSERT INTO DOG VALUES('customer3@email.com',3, 'Lab', 'Max', 2);
+INSERT INTO DOG VALUES('customer4@email.com',2, 'Poodle', 'Fluffy', 8);
+INSERT INTO DOG VALUES('customer2@email.com',1, 'Golden', 'Cooper', 7);
+
+INSERT INTO EMPLOYEE VALUES(1,'Bob', 'Vance', 28, '123 Green Street');
+INSERT INTO EMPLOYEE VALUES(2,'Angela', 'Martin', 27, '3245 Blue Street');
+INSERT INTO EMPLOYEE VALUES(3,'Kevin', 'Smith', 20, '123 Yellow Street');
+INSERT INTO EMPLOYEE VALUES(4,'Andy', 'Bernard', 28, '765 Blue Street');
+
+INSERT INTO MANAGER VALUES(2, 'Angela', 'Martin');
+
+INSERT INTO ROUTE VALUES(11,1, 1, '123 Blue Street', '756 Green Street', 25);
+INSERT INTO ROUTE VALUES(3,3, 2, '123 Main Street', '789 Main Street', 30);
+INSERT INTO ROUTE VALUES(2,4, 3, '123 Main Street', '789 Yellow Street', 50);
+
+
+
+
+
+
+
+
+
+
+
