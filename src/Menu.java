1923c50cb5bfb81aec4c55fcd1adfed79155d8e0
gvgui <gvgui@asu.edu>
2022-08-08 17:14:18 -0400

    centered buttons in jpanel

--- src/Menu.java
+++ src/Menu.java
@@ -1,22 +1,36 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.BoxLayout;
 import java.awt.FlowLayout;
 import java.awt.event.*;
 
 public class Menu extends JFrame {
     public Menu() {
         JButton addEntry = new JButton("Add Entry");
+        JLabel label = new JLabel("Main Menu", SwingConstants.CENTER);
+        JPanel menu = new JPanel();
+        
+        /*
+         * Button functionalities
+         */
         addEntry.addActionListener(new ActionListener() {
            public void actionPerformed(ActionEvent e) {
                new NewEntry();
                setVisible(false);
            }
         });
-        JLabel label = new JLabel("Main Menu");
         
-        add(label);
-        add(addEntry);
+        
+        //Panel Layout
+        menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
+        menu.add(label);
+        menu.add(addEntry);
+        
+        //Frame Information
+        add(menu);
         setTitle("Main Menu");
         setLayout(new FlowLayout());
         setVisible(true);
--- src/NewEntry.java
+++ src/NewEntry.java
@@ -5,21 +5,24 @@ import java.awt.event.*;
 
 public class NewEntry extends JFrame{
     
+    JFrame frame;
+    
     public NewEntry() {
+        frame = new JFrame();
         JButton back = new JButton("Cancel");
         back.addActionListener(new ActionListener() {
            public void actionPerformed(ActionEvent e) {
                Menu menu = new Menu();
-               System.exit(0);
+               frame.dispose();
            }
         });
         
-        add(back);
-        setTitle("Add an Entry");
-        setLayout(new FlowLayout());
-        setVisible(true);
-        setSize(400,400);
-        addWindowListener(new WindowAdapter()
+        frame.add(back);
+        frame.setTitle("Add an Entry");
+        frame.setLayout(new FlowLayout());
+        frame.setVisible(true);
+        frame.setSize(400,400);
+        frame.addWindowListener(new WindowAdapter()
         {
             @Override
             public void windowClosing(WindowEvent e)
